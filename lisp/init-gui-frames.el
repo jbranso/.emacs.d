2f5f6e6ab5ca23f5364851b01f5f31b5073932e0
Joshua Branson <jbranso@purdue.edu>
2015-02-09 12:05:13 -0500

    i am making more changes.

--- lisp/init-gui-frames.el
+++ lisp/init-gui-frames.el
@@ -1,27 +1,10 @@
 ;;----------------------------------------------------------------------------
-;; Stop C-z from minimizing windows under OS X
-;;----------------------------------------------------------------------------
-(defun maybe-suspend-frame ()
-  (interactive)
-  (unless (and *is-a-mac* window-system)
-    (suspend-frame)))
-
-(global-set-key (kbd "C-z") 'maybe-suspend-frame)
-
-
-;;----------------------------------------------------------------------------
 ;; Suppress GUI features
 ;;----------------------------------------------------------------------------
 (setq use-file-dialog nil)
 (setq use-dialog-box nil)
 (setq inhibit-startup-screen t)
 (setq inhibit-startup-echo-area-message t)
-
-
-;;----------------------------------------------------------------------------
-;; Show a marker in the left fringe for lines not in the buffer
-;;----------------------------------------------------------------------------
-(setq indicate-empty-lines t)
 
 ;;----------------------------------------------------------------------------
 ;; Window size and features
