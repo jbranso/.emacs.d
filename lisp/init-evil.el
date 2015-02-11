8730cb9f646862e789b07a48b920239924800660
Joshua Branson <jbranso@purdue.edu>
2015-02-11 16:21:27 -0500

    I have moved my evil-set-initial-state code into init-evil and out of evil-dvorak.

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -29,50 +29,25 @@
 (global-evil-dvorak-mode 1)
 
 ;; (define-key evil-normal-state-map (kbd "Hta") 'web-mode-tag-attributes-sort)
-;; (define-key evil-normal-state-map (kbd "Htb") 'web-mode-tag-beginning)
-;; (define-key evil-normal-state-map (kbd "Hte") 'web-mode-tag-end)
-;; (define-key evil-normal-state-map (kbd "Htm") 'web-mode-tag-match)
-;; (define-key evil-normal-state-map (kbd "Htn") 'web-mode-tag-next)
-;; (define-key evil-normal-state-map (kbd "Htp") 'web-mode-tag-previous)
-;; (define-key evil-normal-state-map (kbd "Hts") 'web-mode-tag-select)
-
-;; (define-key evil-normal-state-map (kbd "Hek") 'web-mode-element-kill)
-;; (define-key evil-normal-state-map (kbd "Hev") 'web-mode-element-vanish)
-;; (define-key evil-normal-state-map (kbd "Hea") 'web-mode-element-content-select)
-;; (define-key evil-normal-state-map (kbd "Hec") 'web-mode-element-clone)
-;; (define-key evil-normal-state-map (kbd "Heb") 'web-mode-element-beginning)
-;; (define-key evil-normal-state-map (kbd "Hed") 'web-mode-element-child)
-;; (define-key evil-normal-state-map (kbd "Hee") 'web-mode-element-end)
-;; (define-key evil-normal-state-map (kbd "Hef") 'web-mode-element-children-fold-or-unfold)
-;; (define-key evil-normal-state-map (kbd "Hei") 'web-mode-element-insert)
-;; (define-key evil-normal-state-map (kbd "Hem") 'web-mode-element-mute-blanks)
-;; (define-key evil-normal-state-map (kbd "Hen") 'web-mode-element-next)
-;; (define-key evil-normal-state-map (kbd "Hep") 'web-mode-element-previous)
-;; (define-key evil-normal-state-map (kbd "Her") 'web-mode-element-rename)
-;; (define-key evil-normal-state-map (kbd "Hes") 'web-mode-element-select)
-;; (define-key evil-normal-state-map (kbd "Het") 'web-mode-element-transpose)
-;; (define-key evil-normal-state-map (kbd "Heu") 'web-mode-element-parent)
-;; (define-key evil-normal-state-map (kbd "Hew") 'web-mode-element-wrap)
-
-;; (define-key evil-normal-state-map (kbd "Hab") 'web-mode-attribute-beginning)
-;; (define-key evil-normal-state-map (kbd "Hae") 'web-mode-attribute-end)
-;; (define-key evil-normal-state-map (kbd "Hai") 'web-mode-attribute-insert)
-;; (define-key evil-normal-state-map (kbd "Hak") 'web-mode-attribute-kill)
-;; (define-key evil-normal-state-map (kbd "Han") 'web-mode-attribute-next)
-;; (define-key evil-normal-state-map (kbd "Has") 'web-mode-attribute-select)
-;; (define-key evil-normal-state-map (kbd "Hat") 'web-mode-attribute-transpose)
-
-;; ;;(define-key evil-emacs-state-map (kbd "C-w h") 'evil-window-down)
-;; (define-key evil-replace-state-map
-;; (define-key evil-visual-state-map
-;; (define-key evil-operator-state-map
-;; ;; Set the default state for various buffers
-;; (evil-set-initial-state 'dired-mode 'emacs)
 ;; (evil-set-initial-state 'snake-mode 'emacs)
-;; (evil-set-initial-state 'eshell-mode 'emacs)
-;; (evil-set-initial-state 'term-mode 'emacs)
-;; ;;This next one is working or not?
-;; (evil-set-initial-state 'git-mode 'emacs)
+
+    (loop for (mode . state) in '((inferior-emacs-lisp-mode . emacs)
+                              (nrepl-mode . insert)
+                              (pylookup-mode . emacs)
+                              (comint-mode . normal)
+                              (shell-mode . insert)
+                              (git-commit-mode . insert)
+                              (git-rebase-mode . emacs)
+                              (term-mode . emacs)
+                              (help-mode . emacs)
+                              (helm-grep-mode . emacs)
+                              (grep-mode . emacs)
+                              (bc-menu-mode . emacs)
+                              (magit-branch-manager-mode . emacs)
+                              (rdictcc-buffer-mode . emacs)
+                              (dired-mode . emacs)
+                              (wdired-mode . normal))
+      do (evil-set-initial-state mode state))
 
 (provide 'init-evil)
 ;; ;;; evil-changes.el ends here
