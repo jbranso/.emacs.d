55a8795ec7fcedd6cdcd12644959b98fa9a2b7e6
Joshua Branson <bransoj@hotmail.com>
2015-10-01 11:31:34 -0400

    I am trying out wanderlust, which is another email client for emacs.

---
+++ lisp/init-wanderlust.el
@@ -0,0 +1,21 @@
+(autoload 'wl "wl" "Wanderlust" t)
+
+(require 'init-wanderlust-secret)
+(setq wl-from "Joshua Branson <jbranson@setyourwaypoint.com>"
+
+      ;;all system folders (draft, trash, spam, etc) are placed in the
+      ;;[Gmail]-folder, except inbox. "%" means it's an IMAP-folder
+      wl-default-folder "%inbox"
+      wl-draft-folder   "%[Gmail]/Drafts"
+      wl-trash-folder   "%[Gmail]/Trash"
+      wl-fcc            "%[Gmail]/Sent"
+
+      ;; mark sent messages as read (sent messages get sent back to you and
+      ;; placed in the folder specified by wl-fcc)
+      wl-fcc-force-as-read    t
+
+      ;;for when auto-compleating foldernames
+      wl-default-spec "%")
+
+
+(provide 'init-wanderlust)
