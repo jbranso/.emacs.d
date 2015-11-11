e262e8e418d7fb83c2b2c21c76f30ee94bc7a54a
Joshua Branson <bransoj@hotmail.com>
2015-11-11 11:15:15 -0500

    I've hopefully allowed helm-mini to use bookmark source.

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -30,7 +30,7 @@
  helm-for-files-preferred-list '(
                                  helm-source-buffers-list
                                  helm-source-recentf
-                                 ;;helm-source-bookmarks
+                                 helm-source-bookmarks
                                  helm-source-file-cache
                                  helm-source-files-in-current-dir
                                  ;;helm-source-locate
