d3b5dce7b010456140fccee2db128621987a4b92
Joshua Branson <bransoj@hotmail.com>
2016-02-29 16:13:30 -0500

    i hopefully buried the "this user joined/this user quit" type irc messages.

--- lisp/init-erc.el
+++ lisp/init-erc.el
@@ -8,26 +8,27 @@
                                       ("freenode.net" "#hurd")
                                       ("freenode.net" "#wordpress")
                                       ;;("freenode.net" "#debian-hurd")
-                                      ))
-  ;; Rename server buffers to reflect the current network name instead
-  ;; of SERVER:PORT. (e.g. "freenode" instead of "irc.freenode.net:6667"). This
-  ;; is useful when using a bouncer like ZNC where you have multiple
-  ;; connections to the same server.
-  (setq erc-rename-buffers t)
-
-  (setq
-   ;; set the default nick
-   erc-nick "joshuaBPMan"
-   ;; don't prompt me for my password erc should know it
-   erc-prompt-for-password nil)
+                                      )
+        ;; Rename server buffers to reflect the current network name instead
+        ;; of SERVER:PORT. (e.g. "freenode" instead of "irc.freenode.net:6667"). This
+        ;; is useful when using a bouncer like ZNC where you have multiple
+        ;; connections to the same server.
+        erc-rename-buffers t
+        ;; set the default nick
+        erc-nick "joshuaBPMan"
+        ;; don't prompt me for my password erc should know it
+        erc-prompt-for-password nil
+        erc-hide-list '("JOIN" "PART" "QUIT")
+        ;; don't let erc switch the active buffer when it connects to a new channel when erc is starting up
+        ;; this gets annoying because when I'm starting up emacs, it'll switch to "#arch", then "#hurd", then "#org-mode".
+        ;; it's very weird.  But after erc has loaded and those initial buffers come up, I want erc to jump to a new channel
+        ;; when I try to join it
+        erc-join-buffer 'bury
+        )
 
   ;; Interpret mIRC-style color commands in IRC chats
   (setq erc-interpret-mirc-color t)
-  ;; don't let erc switch the active buffer when it connects to a new channel when erc is starting up
-  ;; this gets annoying because when I'm starting up emacs, it'll switch to "#arch", then "#hurd", then "#org-mode".
-  ;; it's very weird.  But after erc has loaded and those initial buffers come up, I want erc to jump to a new channel
-  ;; when I try to join it
-  (setq erc-join-buffer 'bury)
+  (setq )
   :config
   (setq erc-join-buffer 'buffer))
 
